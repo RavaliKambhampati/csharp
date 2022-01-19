6ce9caa48809cf288e29bf3e33ce154019f1a394
ravali <kambhampatiravali96@gmail.com>
2022-01-19 15:56:59 +0530

    second commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	class Program
+	{
+		public void calculation()
+		{
+			
+			Console.WriteLine("enter a priciple");
+			double p = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("enter time:");
+			double t = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("enter rateof interest:");
+			double r = Convert.ToDouble(Console.ReadLine());
+			double simpleinterest = p * t * r / 100;
+			Console.WriteLine("simpleinterest is:"+simpleinterest);
+		}
+		static void Main(string[] args)
+		{
+			Program objpr = new Program();
+			objpr.calculation();
+			Console.Read();
+
+		}
+	}
+}
